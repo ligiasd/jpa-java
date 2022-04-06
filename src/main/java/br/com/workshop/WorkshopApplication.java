d6abdd2abab8c27cb5654127daf1bb31779f9f0e
Ligiane <ligiane.alves.souza@gmail.com>
2022-04-06 18:45:32 -0300

    add packages Entities, Resources

--- src/main/java/br/com/workshop/WorkshopApplication.java
+++ src/main/java/br/com/workshop/WorkshopApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WorkshopApplication {
+public class  WorkshopApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(WorkshopApplication.class, args);
 	}
 
---
+++ src/main/java/br/com/workshop/resources/UserResource.java
@@ -0,0 +1,19 @@
+package br.com.workshop.resources;
+
+import br.com.workshop.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Ligiane", "ligiane@email.com","999999","123456");
+        return ResponseEntity.ok().body(u);
+
+    }
+}
